47ed3f917eb63b7e3dac81eac0242d4348af0312
sloth101 <suppe.wert@posteo.de>
2014-07-02 22:28:51 +0200

    2014-07-02 - GUI-Update Selina
    SuperSambaSommerGUI

--- SuperSambaSommerTippspiel/Form1.Designer.cs
+++ SuperSambaSommerTippspiel/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.tb_Name = new System.Windows.Forms.TextBox();
@@ -39,67 +40,97 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(47, 34);
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(409, 94);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(55, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "Name";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(50, 122);
+            this.label2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(399, 158);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.Size = new System.Drawing.Size(77, 20);
             this.label2.TabIndex = 1;
             this.label2.Text = "Passwort";
             // 
             // tb_Name
             // 
-            this.tb_Name.Location = new System.Drawing.Point(50, 51);
+            this.tb_Name.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tb_Name.Location = new System.Drawing.Point(354, 119);
+            this.tb_Name.Margin = new System.Windows.Forms.Padding(5);
             this.tb_Name.Name = "tb_Name";
-            this.tb_Name.Size = new System.Drawing.Size(100, 20);
+            this.tb_Name.Size = new System.Drawing.Size(164, 28);
             this.tb_Name.TabIndex = 2;
             // 
             // tb_Pw
             // 
-            this.tb_Pw.Location = new System.Drawing.Point(53, 139);
+            this.tb_Pw.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tb_Pw.Location = new System.Drawing.Point(354, 183);
+            this.tb_Pw.Margin = new System.Windows.Forms.Padding(5);
             this.tb_Pw.Name = "tb_Pw";
-            this.tb_Pw.Size = new System.Drawing.Size(100, 20);
+            this.tb_Pw.Size = new System.Drawing.Size(164, 28);
             this.tb_Pw.TabIndex = 3;
             // 
             // btn_Anmelden
             // 
-            this.btn_Anmelden.Location = new System.Drawing.Point(53, 197);
+            this.btn_Anmelden.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_Anmelden.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Anmelden.FlatAppearance.BorderColor = System.Drawing.Color.LimeGreen;
+            this.btn_Anmelden.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Anmelden.Location = new System.Drawing.Point(354, 221);
+            this.btn_Anmelden.Margin = new System.Windows.Forms.Padding(5);
             this.btn_Anmelden.Name = "btn_Anmelden";
-            this.btn_Anmelden.Size = new System.Drawing.Size(75, 23);
+            this.btn_Anmelden.Size = new System.Drawing.Size(164, 35);
             this.btn_Anmelden.TabIndex = 4;
             this.btn_Anmelden.Text = "Anmelden";
-            this.btn_Anmelden.UseVisualStyleBackColor = true;
+            this.btn_Anmelden.UseVisualStyleBackColor = false;
             this.btn_Anmelden.Click += new System.EventHandler(this.btn_Anmelden_Click);
             // 
             // btn_Reg
             // 
-            this.btn_Reg.Location = new System.Drawing.Point(155, 197);
+            this.btn_Reg.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_Reg.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Reg.FlatAppearance.BorderColor = System.Drawing.Color.LimeGreen;
+            this.btn_Reg.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Reg.Location = new System.Drawing.Point(354, 266);
+            this.btn_Reg.Margin = new System.Windows.Forms.Padding(5);
             this.btn_Reg.Name = "btn_Reg";
-            this.btn_Reg.Size = new System.Drawing.Size(75, 23);
+            this.btn_Reg.Size = new System.Drawing.Size(164, 35);
             this.btn_Reg.TabIndex = 5;
             this.btn_Reg.Text = "Registrieren";
-            this.btn_Reg.UseVisualStyleBackColor = true;
+            this.btn_Reg.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(793, 595);
+            this.ControlBox = false;
             this.Controls.Add(this.btn_Reg);
             this.Controls.Add(this.btn_Anmelden);
             this.Controls.Add(this.tb_Pw);
             this.Controls.Add(this.tb_Name);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.TopMost = true;
             this.ResumeLayout(false);
             this.PerformLayout();
 
